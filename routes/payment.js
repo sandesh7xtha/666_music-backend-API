118e8aac5a21d1af358dffb4452ab11b496b7f95
sandesh xtha <sandesh7xtha@gmail.com>
2022-04-17 23:03:37 +0545

    history managed

--- routes/payment.js
+++ routes/payment.js
@@ -55,16 +55,88 @@ router.post("/history", async (req, res) => {
   try {
     let data = req.body;
 
-    var sql = "INSERT INTO history SET payment_id =?,sp_id =?";
+    var sql = "INSERT INTO history SET payment_id =?,sp_id =?,quantity=?";
     mysqlconnection.query(
       sql,
-      [data.payment_id, data.sp_id],
+      [data.payment_id, data.sp_id, data.quantity],
       (err, rows, fields) => {
         if (!err) {
           return res.status(200).json({
             status: "ok",
             data: data,
           });
+        } else console.log(err);
+      }
+    );
+  } catch (err) {
+    res.json({
+      message: err,
+    });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    var sql =
+      "SELECT * FROM history join payment on payment.payment_id=history.payment_id join shop_product on shop_product.sp_id=history.sp_id";
+    const output = mysqlconnection.query(sql, (err, result) => {
+      if (!err) {
+        res.status(200).json({
+          status: "ok",
+          data: result,
+        });
+        console.log(result.length);
+        // console.log(output);
+      } else console.log(err);
+    });
+  } catch (err) {
+    res.json({
+      message: err,
+    });
+  }
+});
+
+router.get("/paymentData/:id", async (req, res) => {
+  try {
+    let data = req.body;
+    console.log(data);
+    var sql = "SELECT * FROM payment  where user_id=?";
+    const output = mysqlconnection.query(
+      sql,
+      [req.params.id],
+      (err, result) => {
+        if (!err) {
+          res.status(200).json({
+            status: "ok",
+            data: result,
+          });
+          console.log(result.length);
+          // console.log(output);
+        } else console.log(err);
+      }
+    );
+  } catch (err) {
+    res.json({
+      message: err,
+    });
+  }
+});
+
+router.get("/history/:payment_id", async (req, res) => {
+  try {
+    var sql =
+      "SELECT *,payment.date AS datePayment FROM history join payment on payment.payment_id=history.payment_id join shop_product on shop_product.sp_id=history.sp_id where payment.payment_id=?";
+    const output = mysqlconnection.query(
+      sql,
+      [req.params.payment_id],
+      (err, result) => {
+        if (!err) {
+          res.status(200).json({
+            status: "ok",
+            data: result,
+          });
+          console.log(result.length);
+          // console.log(output);
         } else console.log(err);
       }
     );
